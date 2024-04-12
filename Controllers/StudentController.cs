9cc9816ec23cf8e6c0e6817f5752e6a2c005a330
Pravin Mane <141111800+Pravin7639@users.noreply.github.com>
2024-04-12 23:09:46 +0530

    Delete Record from Table using MVC

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using _03_MVC_CRUD_UsingDBFirstApproch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03_MVC_CRUD_UsingDBFirstApproch.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        StudentDbContext _db = new StudentDbContext();
+        // GET: Student
+        public ActionResult Index()
+        {
+            var students = _db.Students.ToList();
+
+            return View(students);
+        }
+    
+
+        [HttpGet]
+    public ActionResult Delete(int? id)
+        {
+            var student = _db.Students.Find(id);
+            return View(student);
+        }
+
+
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? Id)
+        {
+            var student = _db.Students.Find(Id);
+
+            _db.Students.Remove(student);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
