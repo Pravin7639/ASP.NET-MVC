ce715908f8723abfb0a17d3006366de0fe8a3c5a
Pravin Mane <141111800+Pravin7639@users.noreply.github.com>
2024-04-23 23:21:52 +0530

    Validation In Mvc

---
+++ AccountController.cs
@@ -0,0 +1,53 @@
+﻿using _04_MVC_CRUD_UsingDbFirstApproch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04_MVC_CRUD_UsingDbFirstApproch.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        ProductDbContext _db = new ProductDbContext();
+
+        // GET: Account
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
+
+                    ViewBag.Msg = "User Created Successfully. Log In To Use Website.";
+
+                }
+                else
+                {
+                    ViewBag.Msg = "Please Correct Input";
+
+                }
+
+            }
+            catch
+            {
+                ViewBag.Msg = "Error In Creating User";
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_04_MVC_CRUD_UsingDbFirstApproch.MvcApplication" Language="C#" %>
